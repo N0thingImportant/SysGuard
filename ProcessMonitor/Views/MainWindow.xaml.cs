16ad32904457b0adafa4be1a31bb2658a662a903
N0thingImportant <radomirkhabibullin@gmail.com>
2023-06-11 22:44:06 +0300

    Initial commit

---
+++ ProcessMonitor/Views/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace ProcessMonitor.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.MaxHeight = SystemParameters.MaximizedPrimaryScreenHeight;
+        }
+
+        private void Image_MouseDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
