615911d722c02f13d4c9b8e3fd3cc6044e65078b
N0thingImportant <radomirkhabibullin@gmail.com>
2023-06-17 23:03:40 +0300

    Rename assembly

--- ProcessMonitor/Properties/AssemblyInfo.cs
+++ ProcessMonitor/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProcessMonitor")]
+[assembly: AssemblyTitle("SysGuard")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProcessMonitor")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyProduct("SysGuard")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
