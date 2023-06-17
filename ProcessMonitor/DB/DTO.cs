06030e8a5978a77e83b32b4da49915cd210b70cb
N0thingImportant <radomirkhabibullin@gmail.com>
2023-06-17 23:40:07 +0300

    [WIP] Add DB context and initial db file

---
+++ ProcessMonitor/DB/DTO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class SysFile
+{
+    public int ID { get; set; }
+    public string Path { get; set; }
+    public bool IsRelative { get; set; }
+
+    public string FullPath => IsRelative ? Environment.ExpandEnvironmentVariables(Path) : Path;
+
+    public override string ToString()
+    {
+        return $"{nameof(SysFile)}[{FullPath}]";
+    }
+}
+
+public class SusArg
+{
+    public int ID { get; set; }
+    public string Executable { get; set; }
+    public string CommandLine { get; set; }
+    public bool IsRegex { get; set; }
+
+    public override string ToString()
+    {
+        if (IsRegex)
+            return $"{nameof(SusArg)}[{Executable} /{CommandLine}/]";
+        else
+            return $"{nameof(SusArg)}[{Executable} *{CommandLine}*]";
+    }
+}
+
+public class ProcInfo
+{
+    public int ID { get; set; }
+    public string Executable { get; set; }
+    public string CommandLine { get; set; }
+
+    public override string ToString()
+    {
+        return $"{nameof(ProcInfo)}[{Executable} {(CommandLine?.Length > 100 ? CommandLine.Remove(100) + "..." : CommandLine)}]";
+    }
+}
+
+public enum ActionType
+{
+    None,
+    Start,
+    Stop,
+    SysFile,
+    SusArgs,
+}
+
+public class Log
+{
+    public int ID { get; set; }
+    public ProcInfo Process { get; set; }
+    public int PID { get; set; }
+    public ProcInfo Parent { get; set; }
+    public int? ParentPID { get; set; }
+    public DateTime DateTime { get; set; }
+    public ActionType Action { get; set; }
+    public virtual SysFile File { get; set; }
+
+    public virtual SusArg Args { get; set; }
+
+    public override string ToString()
+    {
+        return $"{nameof(Log)}[{PID}:{Process.Executable} {DateTime} {Action} {(Action == ActionType.SusArgs ? Args.ToString() : Action == ActionType.SysFile ? File.ToString() : "")}";
+    }
+}
---
+++ ProcessMonitor/DB/SysGuardDbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.Entity;
+using System.Data.SQLite;
+using System.Data.SQLite.EF6;
+
+public class SysGuardDbContext : DbContext
+{
+    public DbSet<SysFile> SysFiles { get; set; }
+    public DbSet<SusArg> SusArgs { get; set; }
+    public DbSet<ProcInfo> ProcInfos { get; set; }
+    public DbSet<Log> Logs { get; set; }
+
+    private static SQLiteConnection CreateConnection(string path)
+    {
+        var builder = (SQLiteConnectionStringBuilder)SQLiteProviderFactory.Instance.CreateConnectionStringBuilder();
+        if (builder == null)
+            return null;
+
+        builder.DataSource = path;
+        builder.FailIfMissing = false;
+        return new SQLiteConnection(builder.ToString());
+    }
+
+    public SysGuardDbContext(string path) : base(CreateConnection(path), false) { }
+
+    protected override void OnModelCreating(DbModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<SysFile>().ToTable(nameof(SysFiles));
+        modelBuilder.Entity<SusArg>().ToTable(nameof(SusArgs));
+        modelBuilder.Entity<ProcInfo>().ToTable(nameof(ProcInfos));
+        modelBuilder.Entity<Log>().ToTable(nameof(Logs));
+    }
+}
