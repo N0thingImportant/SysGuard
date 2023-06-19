ec212a61816b62b1e5a95a6114e1ed59d183f05d
N0thingImportant <radomirkhabibullin@gmail.com>
2023-06-19 17:24:24 +0300

    Add Notification

--- ProcessMonitor/DB/SysGuardDbContext.cs
+++ ProcessMonitor/DB/SysGuardDbContext.cs
@@ -2,33 +2,36 @@
 using System.Data.SQLite;
 using System.Data.SQLite.EF6;
 
-public class SysGuardDbContext : DbContext
+namespace ProcessMonitor.DB
 {
-    public DbSet<SysFile> SysFiles { get; set; }
-    public DbSet<SusArg> SusArgs { get; set; }
-    public DbSet<ProcInfo> ProcInfos { get; set; }
-    public DbSet<Log> Logs { get; set; }
-    public DbSet<Reserve> Reserved { get; set; }
-
-    private static SQLiteConnection CreateConnection(string path)
+    public class SysGuardDbContext : DbContext
     {
-        var builder = (SQLiteConnectionStringBuilder)SQLiteProviderFactory.Instance.CreateConnectionStringBuilder();
-        if (builder == null)
-            return null;
+        public DbSet<SysFile> SysFiles { get; set; }
+        public DbSet<SusArg> SusArgs { get; set; }
+        public DbSet<ProcInfo> ProcInfos { get; set; }
+        public DbSet<Log> Logs { get; set; }
+        public DbSet<Reserve> Reserved { get; set; }
 
-        builder.DataSource = path;
-        builder.FailIfMissing = false;
-        return new SQLiteConnection(builder.ToString());
-    }
+        private static SQLiteConnection CreateConnection(string path)
+        {
+            var builder = (SQLiteConnectionStringBuilder)SQLiteProviderFactory.Instance.CreateConnectionStringBuilder();
+            if (builder == null)
+                return null;
 
-    public SysGuardDbContext(string path) : base(CreateConnection(path), false) { }
+            builder.DataSource = path;
+            builder.FailIfMissing = false;
+            return new SQLiteConnection(builder.ToString());
+        }
 
-    protected override void OnModelCreating(DbModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<SysFile>().ToTable(nameof(SysFiles));
-        modelBuilder.Entity<SusArg>().ToTable(nameof(SusArgs));
-        modelBuilder.Entity<ProcInfo>().ToTable(nameof(ProcInfos));
-        modelBuilder.Entity<Log>().ToTable(nameof(Logs));
-        modelBuilder.Entity<Reserve>().ToTable(nameof(Reserved));
+        public SysGuardDbContext(string path) : base(CreateConnection(path), false) { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<SysFile>().ToTable(nameof(SysFiles));
+            modelBuilder.Entity<SusArg>().ToTable(nameof(SusArgs));
+            modelBuilder.Entity<ProcInfo>().ToTable(nameof(ProcInfos));
+            modelBuilder.Entity<Log>().ToTable(nameof(Logs));
+            modelBuilder.Entity<Reserve>().ToTable(nameof(Reserved));
+        }
     }
 }
---
+++ ProcessMonitor/Notification.cs
@@ -0,0 +1,13 @@
+﻿namespace ProcessMonitor
+{
+    static class Notification
+    {
+        public static void Send(string title, string text)
+        {
+            new Microsoft.Toolkit.Uwp.Notifications.ToastContentBuilder()
+                .AddAttributionText(text)
+                .AddText(title)
+                .Show();
+        }
+    }
+}
